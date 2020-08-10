bd8d00087ea98b8b6b7ba255947c921cc725f003
Richard Chorney <32713553+RichardChorney@users.noreply.github.com>
2020-08-10 00:40:32 -0700

    My git versions got out of order so this commit is to change back a few of the menu items that got removed from the last merge. The menu should be back to how it was plus the selections for the new stuff just added

--- src/main/java/edu/pdx/agileteam7/App.java
+++ src/main/java/edu/pdx/agileteam7/App.java
@@ -1,5 +1,6 @@
 package edu.pdx.agileteam7;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -10,6 +11,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.transfer.MultipleFileDownload;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 //import com.sun.tools.javac.comp.Env;
 import java.io.*;
 import javax.sound.midi.SysexMessage;
@@ -31,19 +35,6 @@ public class App {
     public static AmazonS3 S3;
 
     public static void main(String[] args) {
-        final String USAGE = "\n" +
-                "Commands \n" +
-                "q: to quit\n" +
-                "ls: list buckets\n" +
-                "cb: to create new bucket\n" +
-                "gb: to get a bucket\n" +
-                "mkdir: make directory\n" +
-                "cp: copy directory\n" +
-                "adfl: add 1 file to bucket\n" +
-                "adMfl: adds mult. files\n" +
-                "list: list dictionaries and files in local machine\n" +
-                "rename: rename file in local machine\n";
-
         final String Main_Menu = "\n" +
                 "You are in the main menu\n" +
                 "Commands \n" +
@@ -55,6 +46,7 @@ public class App {
                 "You are in the remote menu\n" +
                 "Commands \n" +
                 "b: to go back\n" +
+                "ls: list buckets\n" +
                 "cb: to create new bucket\n" +
                 "gb: to get a bucket\n" +
                 "vgl: view logs of buckets\n" +
@@ -64,6 +56,19 @@ public class App {
                 "b: to go back\n" +
                 "list: list dictionaries and files in local machine\n" +
                 "rename: rename file in local machine\n";
+
+        final String bucketMenu= "\n" +
+                "Please enter one of the following commands.\n" +
+                "b: return to the previous menu\n" +
+                "mkdir: make directory\n" +
+                "cp: copy directory\n" +
+                "adfl: add 1 file to bucket\n" +
+                "adMfl: adds mult. files\n" +
+                "ls: list the objects in the current bucket\n" +
+                "go: Downloads the object to local machine\n" +
+                "gm: Downloads multiple objects to local machine\n" +
+                "gd: Downloads directory to local machine\n" +
+                "chper: change permission for file";
 
         // Asks for user input
         String newestCommand = "";
@@ -82,6 +87,8 @@ public class App {
                 } catch (IOException e) {
                     System.out.println("Unable to retrieve credentials");
                 }
+            } else {
+                System.out.println("Credentials file not used..");
             }
         }
 
@@ -163,19 +170,12 @@ public class App {
                         }
 
                         if (CURRENT_BUCKET != null && !vglAccessed) {
+                            System.out.println("\nBucket accessed.");
                             while (true) {
-                                System.out.println("\nBucket accessed. Please enter one of the following commands.\n" +
-                                        "b: return to the previous menu\n" +
-                                        "mkdir: make directory\n" +
-                                        "cp: copy directory\n" +
-                                        "adfl: add 1 file to bucket\n" +
-                                        "adMfl: adds mult. files\n" +
-                                        "ls: list the objects in the current bucket\n" +
-                                        "go: Downloads the object to local machine\n" +
-                                        "gm: Downloads multiple objects to local machine\n" +
-                                        "chper: Change object permissions\n");
+                                System.out.println(bucketMenu);
                                 newestCommand = myObj.nextLine();
                                 if (newestCommand.equals("b")) {
+                                    CURRENT_BUCKET = null;
                                     break;
                                 } else if (newestCommand.equals("ls")) {
                                     Buckets.listObjects(CURRENT_BUCKET.getName());
@@ -208,15 +208,21 @@ public class App {
                                     } catch (NumberFormatException e) {
                                         System.out.println("Invalid number. Please enter a whole number <= 5");
                                     }
-                                } else if (newestCommand.equals("mkdir")) {
-
-                                    //User Input
-                                    System.out.println("Please enter bucket: ");
-                                    String bucketName = myObj.nextLine();
+                                } else if (newestCommand.equals("gd")) {
+                                    System.out.println("Please enter the name of the remote directory you want to copy(must end with / ): ");
+                                    String remoteDirectory = myObj.nextLine();
+                                    System.out.println("Please enter the name of the local directory you want to be copied into (must end with / ): ");
+                                    String localDirectory = myObj.nextLine();
+                                    try {
+                                        downloadDirectory(CURRENT_BUCKET.getName(), remoteDirectory, localDirectory);
+                                    } catch (Exception e) {
+                                        System.err.println("Directory could not be downloaded. Received the following error: " + e.getMessage());
+                                    }
+                                }else if (newestCommand.equals("mkdir")) {
                                     System.out.println("Please enter directory (must end with / ): ");
                                     String directoryName = myObj.nextLine();
                                     //Calls directory creation function
-                                    boolean success = Directory.mkdir(bucketName, directoryName);
+                                    boolean success = Directory.mkdir(CURRENT_BUCKET.getName(), directoryName);
                                     //Barks failure on function returning an error
                                     if (!success) {
                                         System.out.println("Directory creation failed.");
@@ -384,14 +390,27 @@ public class App {
             }
             s3is.close();
             fos.close();
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
-            return false;
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+        } catch (Exception e) {
             return false;
         }
         return true;
+    }
+    private static void downloadDirectory(String bucket_name, String key_prefix, String dir_path) throws IllegalArgumentException{
+        if(!S3.doesObjectExist(CURRENT_BUCKET.getName(),key_prefix)){
+            throw new IllegalArgumentException("Remote directory does not exist");
+        }
+        System.out.println("downloading to directory: " + dir_path);
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(App.AWS_ACCESS_KEYS, App.AWS_SECRET_KEYS);
+        AmazonS3Client s3 = new AmazonS3Client(awsCreds);
+        TransferManager transferManager = new TransferManager(s3);
+
+        try {
+            MultipleFileDownload transfer = transferManager.downloadDirectory(bucket_name, key_prefix, new File(dir_path));
+            transfer.waitForCompletion();
+        } catch (Exception e) {
+            System.err.println("Unable to download directory. Received the following error: " + e.getMessage());
+        }
+        transferManager.shutdownNow();
     }
 
     public static boolean checkForCredentials() throws IOException {
